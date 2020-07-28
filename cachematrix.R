e85128e0142173e4b7038a7c44c2edd30404e831
raima2000 <65860814+raima2000@users.noreply.github.com>
2020-07-28 22:57:32 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -13,3 +13,43 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+makeCacheMatrix<-function(x=matrix()){
+  inv<-NULL
+  set<-function(y){
+    x<<-y
+    inv<-NULL
+  }
+  get<-function(){x}
+  setInverse<-function(inverse){inv<<-inverse}
+  getInverse<-function(){inv}
+  list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
+}
+
+cachesolve<-function(x,...){
+  inv<-x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  mat<-x$get()
+  inv<-solve(mat,...)
+  x$setInverse(inv)
+  inv
+}
+> source("makeCacheMatrix.R")
+pmatrix<-makeCacheMatrix(matrix(1:4,nrow=2,ncol=2))
+> pmatrix$get()
+     [,1] [,2]
+[1,]    1    3
+[2,]    2    4
+> pmatrix$getinverse()
+Error: attempt to apply non-function
+> cachesolve(pmatrix)
+     [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> pmatrix$getInverse()
+     [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
